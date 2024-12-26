891f166b3364192cf8d0214deffbb2c0154974cb
fsimoes <francisconfqsimoes@gmail.com>
2024-12-26 14:27:20 +0100

    railway cbn: cpds added succesfully

--- experiments/_preprocess_railway_data.py
+++ experiments/_preprocess_railway_data.py
@@ -45,22 +45,26 @@ def cbn_from_railway_data(
     for node in cbn.nodes:
         parents: list[str] = cbn.get_parents(node)
         if len(parents) == 0:
-            node_cpd_table = _normalized_discrete_gamma(k, theta, x_card=max_delay)
+            node_cpd_array = _normalized_discrete_gamma(k, theta, x_card=max_delay)
+            node_cpd_table = node_cpd_array.reshape(-1, 1)
+            evidence = []
+            evidence_card = []
         if len(parents) == 1:
-            node_cpd_table = np.zeros((max_delay,) * 2)  # (node, parent)
-            for p in range(node_cpd_table.shape[1]):
-                node_cpd_table[:, p] = _normalized_discrete_gamma(
-                    k, theta, x_card=node_cpd_table.shape[0], shift=p
+            node_cpd_array = np.zeros((max_delay,) * 2)  # (node, parent)
+            for p in range(node_cpd_array.shape[1]):
+                node_cpd_array[:, p] = _normalized_discrete_gamma(
+                    k, theta, x_card=node_cpd_array.shape[0], shift=p
                 )
+            node_cpd_table = node_cpd_array.reshape(max_delay, -1)
+            evidence = parents
+            evidence_card = [max_delay]
         elif len(parents) == 2:
             node_time: float = _time_to_float(node[-5:])
             same_train_parent_idx: int = _get_same_train_parent_idx(node, parents)
-            # same_train_parent: str = parent[same_train_parent_idx]
-            # same_train_time: int = _time_to_float(same_train_parent[-5:])
             other_train_parent: str = parents[0 if same_train_parent_idx == 1 else 1]
             other_train_time: float = _time_to_float(other_train_parent[-5:])
-            node_cpd_table = np.zeros((max_delay,) * 3)  # (node, parent, parent)
-            for p0, p1 in np.ndindex(*node_cpd_table.shape[1:]):
+            node_cpd_array = np.zeros((max_delay,) * 3)  # (node, parent, parent)
+            for p0, p1 in np.ndindex(*node_cpd_array.shape[1:]):
                 if same_train_parent_idx == 1:  # p1 is value of same_train_parent
                     same_train_delay = p1
                     other_train_delay = p0
@@ -70,16 +74,27 @@ def cbn_from_railway_data(
                 # CPD cases:
                 if node_time - (other_train_time + other_train_delay) < buffer:
                     # Need to ensure buffer
-                    node_cpd_table[:, p0, p1] = _normalized_discrete_gamma(
+                    node_cpd_array[:, p0, p1] = _normalized_discrete_gamma(
                         k,
                         theta,
-                        x_card=node_cpd_table.shape[0],
+                        x_card=node_cpd_array.shape[0],
                         shift=max(buffer, same_train_delay),
                     )
                 else:
-                    node_cpd_table[:, p0, p1] = _normalized_discrete_gamma(
-                        k, theta, x_card=node_cpd_table.shape[0], shift=same_train_delay
+                    node_cpd_array[:, p0, p1] = _normalized_discrete_gamma(
+                        k, theta, x_card=node_cpd_array.shape[0], shift=same_train_delay
                     )
+            node_cpd_table = node_cpd_array.reshape(max_delay, -1)
+            evidence = parents
+            evidence_card = [max_delay] * 2
+        cpd = TabularCPD(
+            variable=node,
+            variable_card=max_delay,
+            values=node_cpd_table,
+            evidence=evidence,
+            evidence_card=evidence_card,
+        )
+        cbn.add_cpds(cpd)
 
     return cbn
 
@@ -119,3 +134,29 @@ if __name__ == "__main__":
     graph = cbn.to_graphviz()
     graph.draw("railway_graph.png", prog="dot")
     Image.open("railway_graph.png").show()
+
+# TEST for way I'm mapping array to table
+# def map_index_to_number(n):
+#     """
+#     Maps all pairs of indices (i, j) where 0 <= i, j < n to unique numbers
+#     in the order they appear in a lexicographical manner.
+
+#     :param n: Maximum index value (the range is 0 to n-1)
+#     :return: Dictionary mapping (i, j) to corresponding number
+#     """
+#     # Create all index pairs (i, j) where 0 <= i, j < n
+#     pairs = [(i, j) for i in range(n) for j in range(n)]
+
+#     # Create a dictionary mapping each pair to its corresponding number
+#     pair_map = {pair: idx for idx, pair in enumerate(pairs)}
+
+#     return pair_map
+#
+# test = node_cpd_table[:5, :5, :5]
+
+# for i, j in np.ndindex(*test.shape[1:]):
+#     print('=====')
+#     print(i,j)
+#     print(test[:, i, j], '\n')
+#     print(test.reshape(5,-1)[:,to_idxs[(i,j)]])
+# to_idxs = map_index_to_number(5)
