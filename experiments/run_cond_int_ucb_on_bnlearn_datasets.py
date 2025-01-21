8521862c4fd1d3e0f9215feb8b90ff9a4302fd94
fsimoes <francisconfqsimoes@gmail.com>
2025-01-21 11:49:58 +0100

    sachs and other local exps

--- experiments/run_cond_int_ucb_on_bnlearn_datasets.py
+++ experiments/run_cond_int_ucb_on_bnlearn_datasets.py
@@ -14,22 +14,22 @@ from _utils import get_node_with_most_ancestors, most_common_list_element
 
 logger.setLevel(logging.ERROR)  # No warnings from pgmpy, only errors
 
-#MAX_WORKERS = 5  # For parallelization (LOCAL)
-MAX_WORKERS = 30  # For parallelization (CLUSTER)
+MAX_WORKERS = 5  # For parallelization (LOCAL)
+# MAX_WORKERS = 30  # For parallelization (CLUSTER)
 
 DATASETS_TARGETS_DCT = {
     # Smaller:
-    #"asia": "dysp",
-    #"cancer": "Dyspnoea",
-    #"sachs": "Akt",
-    #"survey": "T",  # categorical
+    # "asia": "dysp",
+    "cancer": "Dyspnoea",
+    # "sachs": "Akt",
+    # "survey": "T",  # categorical
     # Larger:
-    #"alarm": "BP",
-    #"barley": "protein",  # too big
-    "child": "LowerBodyO2",
-    #"insurance": "PropCost",
-    #"mildew": "dm_4",
-    "water": "CBODD_12_45",
+    # "alarm": "BP",
+    # "barley": "protein",  # too big
+    # "child": "LowerBodyO2",
+    # "insurance": "PropCost",
+    # "mildew": "dm_4",
+    # "water": "CBODD_12_45",
 }
 
 # # Uncomment to check that chosen targets are indeed nodes with most ancestors
@@ -39,10 +39,12 @@ DATASETS_TARGETS_DCT = {
 #     chosen = get_node_with_most_ancestors(bn.to_directed(), no_single_children=True)
 #     print(name, chosen, target)
 
-N_RUNS = 300  # graphs will average over the N_RUNS runs.
+N_RUNS = 500  # graphs will average over the N_RUNS runs.
+# N_RUNS = 300  # graphs will average over the N_RUNS runs.
 # N_RUNS = 2  # graphs will average over the N_RUNS runs.
-#N_ROUNDS = 1000  # number of rounds in each run (SMALLER datasets)
-N_ROUNDS = 10000  # number of rounds in each run (LARGER datasets)
+# N_ROUNDS = 1000  # number of rounds in each run (SMALLER datasets)
+# N_ROUNDS = 10000  # number of rounds in each run (LARGER datasets)
+N_ROUNDS = 1000  # number of rounds in each run (LARGER datasets)
 # N_ROUNDS = 2  # number of rounds in each run
 
 
